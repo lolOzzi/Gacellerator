41e57e8b63ee6ef4ca46cd5222e808333f0f0691
Oliver5811 <88721999+Oliver5811@users.noreply.github.com>
2024-11-30 15:37:44 +0100

    f

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -16,13 +16,13 @@ class Accelerator extends Module {
 
 
   //State enum and register
-  val idle :: loopx :: loopy :: erode :: erodeRight :: erodeLeft :: erodeDown :: erodeUp :: write :: done :: Nil = Enum(10)
+  val idle :: writeBorder :: loopx :: erode :: erodeRight :: erodeLeft :: erodeDown :: erodeUp :: write :: done :: Nil = Enum(10)
   val stateReg = RegInit(idle)
 
   //Support registers
   val addressReg = RegInit(0.U(16.W))
-  val x = RegInit(0.U(8.W))
-  val y = RegInit(0.U(8.W))
+  val x = RegInit(1.U(8.W))
+  val y = RegInit(1.U(8.W))
   val color = RegInit(0.U(8.W))
   val lastY = RegInit(0.U(8.W))
   val prev = RegInit(VecInit(Seq.fill(20)(0.U(8.W))))
@@ -32,6 +32,7 @@ class Accelerator extends Module {
   val currCell= RegInit(0.U(8.W))
   val currAddress = RegInit(0.U(16.W))
   val currColor = RegInit(0.U(8.W))
+  val writeCell = RegInit(0.U(8.W))
 
   //Default values
   io.writeEnable := false.B
@@ -43,8 +44,27 @@ class Accelerator extends Module {
   switch(stateReg) {
     is(idle) {
       when(io.start) {
-        stateReg := loopx
+        stateReg := writeBorder
         addressReg := 0.U(16.W)
+      }
+    }
+    is(writeBorder){
+      when(writeCell < 76.U){
+        when(writeCell <= 19.U){
+          io.address := 20.U * writeCell + 400.U
+        }.elsewhen(writeCell >= 20.U && writeCell <= 39.U){
+          io.address := 20.U * (writeCell-20.U) + 419.U
+        }.elsewhen(writeCell >= 40.U && writeCell <= 57.U){
+          io.address := (writeCell-40.U) + 400.U + 1.U
+        }.otherwise{
+          io.address := (writeCell-58.U) + 781.U
+        }
+        io.dataWrite := 255.U
+        io.writeEnable := true.B
+        writeCell := writeCell + 1.U
+        when(writeCell === 75.U){
+          stateReg := loopx
+        }
       }
     }
     is(loopx) {
@@ -54,14 +74,11 @@ class Accelerator extends Module {
         io.writeEnable := true.B
       }
 
-      when(y === 20.U) {
-        y := 0.U
+      when(y === 18.U) {
+        y := 1.U
       }
-      when(x === 20.U) {
+      when(x === 19.U) {
         stateReg := done
-      }.elsewhen(x === 0.U | y === 0.U | x === 19.U | y === 19.U) {
-        currCell := 0.U
-        stateReg := write
       }.otherwise {
         stateReg := erode
       }
@@ -86,52 +103,52 @@ class Accelerator extends Module {
       io.address := addressReg - 1.U
       stateReg := Mux(io.dataRead === 255.U, erodeRight, write)
     }
-      is(erodeRight) {
-        io.address := addressReg + 1.U
-        stateReg := Mux(io.dataRead === 255.U, erodeDown, write)
-      }
-      is(erodeDown) {
-        io.address := addressReg + 20.U
-        stateReg := Mux(io.dataRead === 255.U, erodeUp, write)
-      }
-      is(erodeUp) {
-        io.address := addressReg - 20.U
-        color := io.dataRead
-        stateReg := write
+    is(erodeRight) {
+      io.address := addressReg + 1.U
+      stateReg := Mux(io.dataRead === 255.U, erodeDown, write)
+    }
+    is(erodeDown) {
+      io.address := addressReg + 20.U
+      stateReg := Mux(io.dataRead === 255.U, erodeUp, write)
+    }
+    is(erodeUp) {
+      io.address := addressReg - 20.U
+      color := io.dataRead
+      stateReg := write
+    }
+
+    is(write) {
+      when(skip === 1.U) {
+        io.address := addressReg + 400.U - 20.U
+        io.dataWrite := 0.U
+        io.writeEnable := true.B
       }
 
-      is(write) {
-          when(skip === 1.U) {
-            io.address := addressReg + 400.U - 20.U
-            io.dataWrite := 0.U
-            io.writeEnable := true.B
-          }
-
-          currAddress := addressReg + 400.U
-          currColor := color
-          when(currCell === 0.U && y < 18.U) {
-            y := y + 2.U
-            skip := 1.U
-            stateReg := loopx
-            when(y === 19.U) {
-              x := x + 1.U
-            }
-          }.otherwise {
-            y := y + 1.U
-            skip := 0.U
-            stateReg := loopx
-            when(y === 19.U) {
-              x := x + 1.U
-            }
-          }
+      currAddress := addressReg + 400.U
+      currColor := color
+      when(currCell === 0.U && y < 16.U) {
+        y := y + 2.U
+        skip := 1.U
+        stateReg := loopx
+        when(y === 17.U) {
+          x := x + 1.U
         }
-
-
-      is(done) {
-        io.done := true.B
-        stateReg := done
+      }.otherwise {
+        y := y + 1.U
+        skip := 0.U
+        stateReg := loopx
+        when(y === 17.U) {
+          x := x + 1.U
+        }
       }
     }
+
+
+    is(done) {
+      io.done := true.B
+      stateReg := done
+    }
+  }
 
 }
 
